2ab515ce0c45aa6eaaae6eaebab363bb7e9a3d40
AlexMesh <startlist@yandex.ru>
2023-07-27 16:21:36 +0300

    finish

--- Week3/Day5/game.py
+++ Week3/Day5/game.py
@@ -2,19 +2,17 @@ import random
 
 
 class Game:
-    lst_choice = ["rock", "papper", "scissors"]
+    # lst_choice = ["rock", "papper", "scissors"]
 
     def __init__(self):
-        self.result = {"draw": 0, "win": 0, "loss": 0}
+        self.result = {"draw": 0, "win": 0, "lose": 0}
 
     @staticmethod
     def get_user_item():
-        item = (
-            input("Please select an item ((r)rock, (p)paper, (s)scissors)")
-            .strip()
-            .lower[0]
-        )
-        print
+        item = input("Please select an item ((r)rock, (p)paper, (s)scissors)")
+        # .strip()
+        # .lower[0]
+
         return item
 
     @staticmethod
@@ -25,24 +23,27 @@ class Game:
     def get_game_result(self, user_item, computer_item):
         if user_item == computer_item:
             self.result["draw"] += 1
+            return "draw"
         elif (
             (user_item == "r" and computer_item == "s")
             or (user_item == "s" and computer_item == "p")
             or (user_item == "p" and computer_item == "r")
         ):
             self.result["win"] += 1
+            return "win"
         else:
-            self.result["lost"] += 1
-        return self.result
+            self.result["lose"] += 1
+            return "lose"
 
     def play(self):
         user = self.get_user_item()
         computer = self.get_computer_item()
         results = self.get_game_result(user, computer)
+        return results
 
         # - print the output sentence
         # - return the results
 
 
 new_game = Game()
-new_game.play()
+print(new_game.play())
