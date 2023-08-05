5b538e2464def4d99ed9829671abc086901eb3ae
AlexMesh <startlist@yandex.ru>
2023-08-06 00:23:04 +0300

    my game

--- Week4/Day5/MyGame/main.py
+++ Week4/Day5/MyGame/main.py
@@ -1,7 +1,30 @@
 import pygame
+from random import randrange as rnd
 
-WIDTH, HEIGHT = 1280, 800
+WIDTH, HEIGHT = 1200, 800
 fps = 60
+
+# paddle sitting
+paddle_w = 330
+paddle_h = 35
+paddle_speed = 15
+paddle = pygame.Rect(
+    WIDTH // 2 - paddle_w // 2, HEIGHT - paddle_h - 10, paddle_w, paddle_h
+)
+
+# ball setting
+ball_radius = 20
+ball_speed = 7
+ball_rect = int(ball_radius * 2**0.5)
+ball = pygame.Rect(rnd(ball_rect, WIDTH - ball_rect), HEIGHT // 2, ball_rect, ball_rect)
+dx, dy = 1, -1
+# blocks settings
+block_list = [
+    pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)
+]
+color_list = [
+    (rnd(30, 256), rnd(30, 256), rnd(30, 256)) for i in range(10) for j in range(4)
+]
 
 pygame.init()
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -15,6 +38,33 @@ while True:
         if event.type == pygame.QUIT:
             exit()
     sc.blit(img, (0, 0))
+    # drawing world
+    [
+        pygame.draw.rect(sc, color_list[color], block)
+        for color, block in enumerate(block_list)
+    ]
+    pygame.draw.rect(sc, pygame.Color("darkorange"), paddle)
+    pygame.draw.circle(sc, pygame.Color("white"), ball.center, ball_radius)
+    # ball movement
+    ball.x += ball_speed * dx
+    ball.y += ball_speed * dy
+    # collision left right
+    if ball.centerx < ball_radius or ball.centerx > WIDTH - ball_radius:
+        dx = -dx
+    # collision top
+    if ball.centery < ball_radius:
+        dy = -dy
+    # collision paddle
+    if ball.colliderect(paddle) and dy > 0:
+        dy = -dy
+
+    # control
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and paddle.left > 0:
+        paddle.left -= paddle_speed
+    if key[pygame.K_RIGHT] and paddle.right < WIDTH:
+        paddle.right += paddle_speed
 
     # update screen
     pygame.display.flip()
